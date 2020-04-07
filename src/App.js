7006983a5edd7b69119752fe66d31aebc73a6a9d
dhillman1989 <52170803+dhillman1989@users.noreply.github.com>
2020-04-07 15:19:21 +0100

    add react-router
    add react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
   render() {
-    return <Palette palette={generatePalette(seedColors[3])} />;
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>Palette List</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={() => <h1>Single Palette</h1>}
+        />
+      </Switch>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   rootElement
 );
