c9e8e80106f2f5f68dca10970369774607923d5c
dhillman1989 <52170803+dhillman1989@users.noreply.github.com>
2020-04-07 17:10:34 +0100

    start styling MiniPalette
    start styling MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -3,21 +3,40 @@ import { withStyles } from "@material-ui/styles";
 import "./MiniPalette.css";
 
 const styles = {
-  main: {
-    backgroundColor: "purple",
-    border: "3px solid teal",
-    "& h1": {
-      color: "red"
+  root: {
+    width: "200px",
+    backgroundColor: "white",
+    border: "1px solid black",
+    borderRadius: "5px",
+    padding: "0.5rem",
+    position: "relative",
+    overflow: "hidden",
+    "& hover": {
+      cursor: "pointer"
     }
+  },
+  colors: {
+    backgroundColor: "grey"
+  },
+  title: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "0",
+    color: "black",
+    paddingTop: "0.5rem",
+    fontSize: "1rem",
+    position: "relative"
   }
 };
 
 function MiniPalette(props) {
-  const { classes } = props;
+  const { classes, paletteName } = props;
   console.log(classes);
   return (
-    <div className={classes.main}>
-      <h1>MiniPalette</h1>
+    <div className={classes.root}>
+      <div className={classes.colors}>...</div>
+      <h5 className={classes.title}>{paletteName}</h5>
     </div>
   );
 }
